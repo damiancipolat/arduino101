1db0a127a685cb8d8ee173840ee235ce3e3b2871
damian <damian.cipolat@gmail.com>
2020-08-10 22:47:20 -0300

    add

--- display_lcd_i2c/display_lcd_i2c.ino
+++ display_lcd_i2c/display_lcd_i2c.ino
@@ -1,17 +1,25 @@
-#include <LiquidCrystal_I2C.h> //DESCARGAR LIBRERÍA:https://github.com/ELECTROALL/Codigos-arduino/blob/master/LiquidCrystal_I2C.zip
-LiquidCrystal_I2C lcd(0x27,12,6); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,12,6);
+
 void setup() {
   lcd.init();
- lcd.backlight();
+  lcd.backlight();
   lcd.clear();
- lcd.setCursor(0,0);
-  lcd.print("==> Hola <=="); 
-  lcd.setCursor (0,1);
-  lcd.print("==> DAMIAN <");
 }
+
 void loop() { 
+  //Show
   lcd.display();
   delay(500);
+
+  //Write
+  lcd.setCursor(0,0);
+  lcd.print("Hola !!!!"); 
+  lcd.setCursor (0,1);
+  lcd.print("Dami y Fran :D");
+
+  //Hidden    
   lcd.noDisplay();
   delay(500);
 }
